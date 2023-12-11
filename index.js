21c58333c7db450c9c052fd104fcae588bf8b5fb
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-12 02:40:12 +0300

    Arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const nums = [10, 5, 20, 15];
+const max = Math.max(...nums);
